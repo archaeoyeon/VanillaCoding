ca2365191a0a152b071b82d6eeb1ad895f6a514a
archaeoyeon <83646481+archaeoyeon@users.noreply.github.com>
2021-06-15 17:56:12 +0900

    Update Carousel.js

--- vanillaCoding/Carousel.js
+++ vanillaCoding/Carousel.js
@@ -9,8 +9,8 @@ prev.addEventListener("click", clickPrev);
 forward.addEventListener("click", clickForward);
 
 const images = {
-    1 : "https://cdn.pixabay.com/photo/2017/02/04/22/37/panther-2038656__480.png",
-    2 : "https://cdn.pixabay.com/photo/2017/09/01/00/16/png-2702697__480.png",
+    1 : "https://cdn.pixabay.com/photo/2017/09/01/00/16/png-2702697__480.png",
+    2 : "https://cdn.pixabay.com/photo/2017/02/04/22/37/panther-2038656__480.png",
     3 : "https://cdn.pixabay.com/photo/2017/08/27/20/10/png-2687339__480.png",
     4 : "https://cdn.pixabay.com/photo/2016/10/11/18/34/black-1732414__480.png",
     5 : "https://cdn.pixabay.com/photo/2016/03/23/04/57/cat-1274094__480.png"
