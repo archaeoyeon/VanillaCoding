36d2b63ab3d556b6e7b449f3db9801eb09b6d1d1
archaeoyeon <83646481+archaeoyeon@users.noreply.github.com>
2021-06-15 16:22:11 +0900

    Update hex.js

--- vanillaCoding/hex.js
+++ vanillaCoding/hex.js
@@ -2,61 +2,35 @@ const core = document.querySelector(".core"),
     colorcode = core.querySelector("span"),
     click = core.querySelector("input");
 
+
 function hexGenerator() {
-    const hexNumbers = [
-      0,
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      'A',
-      'B',
-      'C',
-      'D',
-      'E',
-      'F'
-    ];
+    const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,'A','B','C','D','E','F'];
   
     let result = "#";
     
-    for (var i = 0; i < 6; i += 1) {
+    for (let i = 0; i < 6; i ++) {
       const randomIndex = Math.floor(Math.random() * hexNumbers.length);
       result += hexNumbers[randomIndex];
     }
     
-    return result;
-  }
-
-
-
-  function changebackground(){
-      core.style.backgroundColor = hexGenerator();
-
-  }
-
-  function changetext (text){
-      colorcode.innerHTML = `${text}`;
-      changebackground(text);
-  }
-
-  function action(event){
-    event.preventDefault();
-    changetext(hexGenerator());
-    
-  }
-
-  function Clickbutton(){
-    click.addEventListener("click", action); 
+  return result;
 }
 
-  function init (){
-    Clickbutton();
-    hexGenerator()
-  }
+function changeBackground(hexCode){
+  core.style.backgroundColor = hexCode;
+}
 
-  init()
+function changeText (text){
+  colorcode.innerHTML = `: ${text}`;
+  changeBackground(text);
+}
+
+function action(){
+  changeText(hexGenerator());
+}
+
+function clickButton(){
+  click.addEventListener("click", action); 
+}
+
+clickButton();
